4f21e965c29eaf944a6426fe045492ae3a9a74b8
asmaylmr117 <asmaylmr117@gmail.com>
2026-05-12 18:10:53 +0300

    update feture

--- src/components/software-company-components/ServiceSection/ServiceSection.js
+++ src/components/software-company-components/ServiceSection/ServiceSection.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { useServices } from '../../../hooks/useQueries';
-import { getImageUrl } from '../../../api/axiosConfig';
-import Bg from '../../../images/shapes/shape_title_under_line.svg'
+
 import shape1 from '../../../images/shapes/shape_line_5.svg'
 import shape2 from '../../../images/shapes/shape_line_6.svg'
 import shape3 from '../../../images/shapes/shape_space_1.svg'
 import shape4 from '../../../images/shapes/shape_angle_1.webp'
 import shape5 from '../../../images/shapes/shape_angle_2.webp'
 
+const ServiceSection = () => {
 
-const ServiceSection = (props) => {
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+    const { isLoading } = useServices();
 
-    const { data: Services, isLoading } = useServices();
-
-    if (isLoading) return <div className="text-center section_space">Loading...</div>;
+    if (isLoading)
+        return <div className="text-center section_space">Loading...</div>;
 
     return (
         <section className="service_section pt-175 pb-80 bg-light section_decoration xb-hidden">
             <div className="container">
-                
             </div>
 
             <div className="decoration_item shape_image_1">
-                <img src={shape1} alt="Techco Shape"/>
+                <img src={shape1} alt="Techco Shape" />
             </div>
+
             <div className="decoration_item shape_image_2">
-                <img src={shape2} alt="Techco Shape"/>
+                <img src={shape2} alt="Techco Shape" />
             </div>
+
             <div className="decoration_item shape_image_3">
-                <img src={shape3} alt="Techco Shape"/>
+                <img src={shape3} alt="Techco Shape" />
             </div>
+
             <div className="decoration_item shape_image_4">
-                <img src={shape4} alt="Techco Shape Angle"/>
+                <img src={shape4} alt="Techco Shape Angle" />
             </div>
+
             <div className="decoration_item shape_image_5">
-                <img src={shape5} alt="Techco Shape Angle"/>
+                <img src={shape5} alt="Techco Shape Angle" />
             </div>
         </section>
     );
